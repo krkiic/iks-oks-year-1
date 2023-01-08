89839b5a45477345dcefd93713fbcb09fc06388b
filip <filip@MASINA>
2023-01-08 20:05:49 +0100

    Add project files.

---
+++ iks oks finalni/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace iks_oks_finalni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.a1.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(41, 86);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(59, 58);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.klik);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.c2.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(205, 185);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(59, 58);
+            this.c2.TabIndex = 1;
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.klik);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.b2.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(123, 185);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(59, 58);
+            this.b2.TabIndex = 2;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.klik);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.a2.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(41, 185);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(59, 58);
+            this.a2.TabIndex = 3;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.klik);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.c1.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(205, 86);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(59, 58);
+            this.c1.TabIndex = 4;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.klik);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.b1.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(123, 86);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(59, 58);
+            this.b1.TabIndex = 5;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.klik);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.c3.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(205, 284);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(59, 58);
+            this.c3.TabIndex = 6;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.klik);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.b3.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(123, 284);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(59, 58);
+            this.b3.TabIndex = 7;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.klik);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.a3.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(41, 284);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(59, 58);
+            this.a3.TabIndex = 8;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.klik);
+            // 
+            // restart
+            // 
+            this.restart.BackColor = System.Drawing.Color.Peru;
+            this.restart.Location = new System.Drawing.Point(71, 402);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(160, 38);
+            this.restart.TabIndex = 9;
+            this.restart.Text = "restart";
+            this.restart.UseVisualStyleBackColor = false;
+            this.restart.Click += new System.EventHandler(this.restartDugme);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(308, 485);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.a1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "iks oks";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button restart;
+    }
+}
+
---
+++ iks oks finalni/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+// od ovde
+namespace iks_oks_finalni
+{
+    public partial class Form1 : Form
+    {
+        bool koigra = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //klik
+        private void klik(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            if (koigra)
+                B.Text = "X";
+            else
+                B.Text = "O";
+
+            koigra = !koigra; // negira
+            //pozvane
+            dugmestop();
+            ceker();
+
+        }
+        private void ceker()
+        {
+            //horizontalno
+
+
+            bool pobednik = false;
+            if (((a1.Text == "X") && (a2.Text == "X")) && (a3.Text == "X"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Red;
+                a2.BackColor = Color.Red;
+                a3.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+
+            }
+
+            if (((a1.Text == "O") && (a2.Text == "O")) && (a3.Text == "O"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Blue;
+                a2.BackColor = Color.Blue;
+                a3.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+            if (((b1.Text == "X") && (b2.Text == "X")) && (b3.Text == "X"))
+            {
+                pobednik = true;
+                b1.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                b3.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((b1.Text == "O") && (b2.Text == "O")) && (b3.Text == "O"))
+            {
+                pobednik = true;
+                b1.BackColor = Color.Blue;
+                b2.BackColor = Color.Blue;
+                b3.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+            if (((c1.Text == "X") && (c2.Text == "X")) && (c3.Text == "X"))
+            {
+                pobednik = true;
+                c1.BackColor = Color.Red;
+                c2.BackColor = Color.Red;
+                c3.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((c1.Text == "O") && (c2.Text == "O")) && (c3.Text == "O"))
+            {
+                pobednik = true;
+                c1.BackColor = Color.Blue;
+                c2.BackColor = Color.Blue;
+                c3.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            //vertikalni
+            if (((a1.Text == "X") && (b1.Text == "X")) && (c1.Text == "X"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Red;
+                b1.BackColor = Color.Red;
+                c1.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((a1.Text == "O") && (b1.Text == "O")) && (c1.Text == "O"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Blue;
+                b1.BackColor = Color.Blue;
+                c1.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+            }
+
+
+            if (((a2.Text == "X") && (b2.Text == "X")) && (c2.Text == "X"))
+            {
+                pobednik = true;
+                a2.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                c2.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((a2.Text == "O") && (b2.Text == "O")) && (c2.Text == "O"))
+            {
+                pobednik = true;
+                a2.BackColor = Color.Blue;
+                b2.BackColor = Color.Blue;
+                c2.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+            }
+
+            if (((a3.Text == "X") && (b3.Text == "X")) && (c3.Text == "X"))
+            {
+                pobednik = true;
+                a3.BackColor = Color.Red;
+                b3.BackColor = Color.Red;
+                c3.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((a3.Text == "O") && (b3.Text == "O")) && (c3.Text == "O"))
+            {
+                pobednik = true;
+                a3.BackColor = Color.Blue;
+                b3.BackColor = Color.Blue;
+                c3.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+            }
+            //ukoso
+            if (((a1.Text == "X") && (b2.Text == "X")) && (c3.Text == "X"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                c3.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((a1.Text == "O") && (b2.Text == "O")) && (c3.Text == "O"))
+            {
+                pobednik = true;
+                a1.BackColor = Color.Blue;
+                b2.BackColor = Color.Blue;
+                c3.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+            }
+
+
+            if (((a3.Text == "X") && (b2.Text == "X")) && (c1.Text == "X"))
+            {
+                pobednik = true;
+                a3.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                c1.BackColor = Color.Red;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+            }
+
+            if (((a3.Text == "O") && (b2.Text == "O")) && (c1.Text == "O"))
+            {
+                pobednik = true;
+                a3.BackColor = Color.Blue;
+                b2.BackColor = Color.Blue;
+                c1.BackColor = Color.Blue;
+                a1.Enabled = false;
+                a2.Enabled = false;
+                a3.Enabled = false;
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                c1.Enabled = false;
+                c2.Enabled = false;
+                c3.Enabled = false;
+
+
+            }
+
+        }
+        private void dugmestop()
+        {
+            if (a1.Text == "X" || a1.Text == "O")
+            {
+                a1.Enabled = false;
+            }
+            if (a2.Text == "X" || a2.Text == "O")
+            {
+                a2.Enabled = false;
+            }
+            if (a3.Text == "X" || a3.Text == "O")
+            {
+                a3.Enabled = false;
+            }
+            if (b1.Text == "X" || b1.Text == "O")
+            {
+                b1.Enabled = false;
+            }
+            if (b2.Text == "X" || b2.Text == "O")
+            {
+                b2.Enabled = false;
+            }
+            if (b3.Text == "X" || b3.Text == "O")
+            {
+                b3.Enabled = false;
+            }
+            if (c1.Text == "X" || c1.Text == "O")
+            {
+                c1.Enabled = false;
+            }
+            if (c2.Text == "X" || c2.Text == "O")
+            {
+                c2.Enabled = false;
+            }
+            if (c3.Text == "X" || c3.Text == "O")
+            {
+                c3.Enabled = false;
+            }
+
+        }
+
+
+        //restart dugme
+
+        private void restartDugme(object sender, EventArgs e)
+        {
+            restart.BackColor = Color.Gold;
+            Application.Restart();
+
+        }
+
+      
+    }
+}
+
+
